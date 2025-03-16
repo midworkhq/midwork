de159d897650f23b1c2fa859d46a48200dc7178e
Onni Hakala <onni@flaky.build>
2025-03-16 22:17:41 +0100

    Lock Github Action tags to commit revs automatically with git hook

--- devenv.nix
+++ devenv.nix
@@ -70,5 +70,28 @@
     files = "secret.*\\.(yaml|yml|json)$";
   };
 
+  # Security hardening to prevent malicious takeover of Github Actions:
+  # https://news.ycombinator.com/item?id=43367987
+  # Replaces tags like "v4" in 3rd party Github Actions to the commit hashes
+  git-hooks.hooks.lock-github-action-tags = {
+    enable = true;
+    files = "^.github/workflows/";
+    types = [ "yaml" ];
+    entry =
+      let
+        script_path = pkgs.writeShellScript "lock-github-action-tags" ''
+          for workflow in "$@"; do
+            grep -E "uses:[[:space:]]+[A-Za-z0-9._-]+/[A-Za-z0-9._-]+@v[0-9]+" "$workflow" | while read -r line; do
+              repo=$(echo "$line" | sed -E 's/.*uses:[[:space:]]+([A-Za-z0-9._-]+\/[A-Za-z0-9._-]+)@v[0-9]+.*/\1/')
+              tag=$(echo "$line" | sed -E 's/.*@((v[0-9]+)).*/\1/')
+              commit_hash=$(git ls-remote "https://github.com/$repo.git" "refs/tags/$tag" | cut -f1)
+              [ -n "$commit_hash" ] && sed -i.bak -E "s|(uses:[[:space:]]+$repo@)$tag|\1$commit_hash #$tag|g" "$workflow" && rm -f "$workflow.bak"
+            done
+          done
+        '';
+      in
+      toString script_path;
+  };
+
   # See full reference at https://devenv.sh/reference/options/
 }
